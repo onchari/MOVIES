d8feba60644ff03b088399721c87da1e3e0714a6
onchari wycliffe <onchariwycliffe@gmail.com>
2017-07-03 19:41:07 +0300

    NEW CHANGES

---
+++ Movies_Store1/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Movies_Store1.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string MovieName { get; set; }
+        public string Genre { get; set; }
+
+        [DisplayFormat(DataFormatString =  "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ Movies_Store1/Models/Movies_Store1Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Movies_Store1.Models
+{
+    public class Movies_Store1Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Movies_Store1Context() : base("name=Movies_Store1Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Movies_Store1.Models.Movie> Movies { get; set; }
+    }
+}
